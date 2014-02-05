5b6131581fe4dbee0778fa5cc57fcf231018b1da
marcoberri <marcoberri@gmail.com>
2014-02-05 09:08:59 +0100

    fix

--- src/it/marcoberri/radiorisaiasud/ImageAdapter.java
+++ src/it/marcoberri/radiorisaiasud/ImageAdapter.java
@@ -14,15 +14,10 @@ public class ImageAdapter extends BaseAdapter {
     private Context mContext;
 
     private Integer[] mThumbIds = {
-<<<<<<< HEAD
             R.drawable.gallery_1,R.drawable.gallery_2,R.drawable.gallery_3,R.drawable.gallery_4,R.drawable.gallery_5};
     
-	public ImageAdapter(Context c) {
-=======
-            R.drawable.gallery_1,R.drawable.gallery_2,R.drawable.gallery_3,R.drawable.gallery_4,R.drawable.gallery_5,R.drawable.gallery_11};
  
    public ImageAdapter(Context c) {
->>>>>>> 35962149f801eff4b3bd456a1352ba1704502965
         mContext = c;
     }
 
@@ -42,28 +37,16 @@ public class ImageAdapter extends BaseAdapter {
 	@SuppressWarnings("deprecation")
 	public View getView(int position, View convertView, ViewGroup parent) {
         ImageView i = new ImageView(mContext);
-<<<<<<< HEAD
+        
         if(position > mThumbIds.length)
         	position = 0;
+        
         if(position < 0)
         	position = 0;
+        
         i.setImageResource(mThumbIds[position]);
         i.setAdjustViewBounds(true);
-        i.setLayoutParams(new Gallery.LayoutParams(
-=======
-
-        if(position > mThumbIds.length)
-        	position = 0;
-        if(position <0)
-        	position = 0;
-     
-        i.setImageResource(mThumbIds[position]);
-        i.setAdjustViewBounds(true);
-       /* i.setLayoutParams(new Gallery.LayoutParams(
->>>>>>> 35962149f801eff4b3bd456a1352ba1704502965
-                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-        //i.setBackgroundResource(R.drawable.picture_frame);
-       
+        i.setLayoutParams(new Gallery.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
         return i;
     }
 
--- src/it/marcoberri/radiorisaiasud/MainActivity.java
+++ src/it/marcoberri/radiorisaiasud/MainActivity.java
@@ -7,11 +7,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Gallery;
 
-public class MainActivity extends Activity  implements OnItemSelectedListener {
+public class MainActivity extends Main   {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +21,7 @@ public class MainActivity extends Activity  implements OnItemSelectedListener {
 	}
 
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+
 	
 	@Override
 	protected void onPause() {
@@ -38,52 +35,6 @@ public class MainActivity extends Activity  implements OnItemSelectedListener {
 		MusicPlayer.pause();
 	}
 	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle item selection
-		switch (item.getItemId()) {
-		case R.id.menu_main_gallery:
-			setContentView(R.layout.activity_gallery);
-			Gallery g = (Gallery) findViewById(R.id.gallery);
-	        g.setAdapter(new ImageAdapter(this));
-	        g.setOnItemSelectedListener(this);			
-			return true;
-		case R.id.menu_main_website:
-			Utils.launchWebBrowser("http://www.radiorisaiasud.it", this);
-			return true;
-		case R.id.menu_main_facebook:
-			Utils.launchFacebookPage("Radio-Risaia-Sud/224882854223371",
-					getPackageManager(), this);
-			return true;
-		case R.id.menu_main_email:
-			return true;			
-		case R.id.action_play:
-			final boolean play = MusicPlayer.toggle(this, R.raw.mulitta_mulit);
-			if (play) {
-				item.setIcon(R.drawable.ic_pause);
-			} else {
-				item.setIcon(R.drawable.ic_play);
-			}
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
-
-
-	@Override
-	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
-			long arg3) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	@Override
-	public void onNothingSelected(AdapterView<?> arg0) {
-		// TODO Auto-generated method stub
-		
-	}
 
 	
 }
